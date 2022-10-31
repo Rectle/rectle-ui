33f7e9900a2b55b0516638061633f928e8bd02aa
Micha Jasiski <michaqu11@wp.pl>
2022-10-31 17:30:07 +0100

    Initial commit

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-h3">Rectle Application</div>
+</template>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <example-component title="Example component" active></example-component>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import ExampleComponent from 'components/ExampleComponent.vue';
+</script>
