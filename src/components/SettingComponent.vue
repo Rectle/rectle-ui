64bf09224b94749f2cfdc79410808bb221868289
Michał Jasiński <michaqu11@wp.pl>
2022-11-21 21:46:45 +0100

    added separators beetwen setting items

--- src/components/SettingComponent.vue
+++ src/components/SettingComponent.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="q-my-md row items-start justify-center text-h4">
     {{ $t('settingPage.title') }}
-  </div> 
+  </div>
+  <q-separator/>
   <q-list class="text-h6" separator>
-      <q-item class="q-py-md" v-for="item of listOfSettings" v-bind:key="item.title">
-       <q-item-section avatar>
-          <q-icon :name="item.icon" />
-        </q-item-section>
-        <q-item-section>{{item.title}}</q-item-section>
-        <q-item-section avatar>
-          <q-icon name="arrow_forward_ios" />
-        </q-item-section>
-      </q-item>
-    </q-list>
+    <q-item class="q-py-lg" v-for="item of listOfSettings" v-bind:key="item.title">
+      <q-item-section avatar>
+        <q-icon :name="item.icon" />
+      </q-item-section>
+      <q-item-section>{{item.title}}</q-item-section>
+      <q-item-section avatar>
+        <q-icon name="arrow_forward_ios" />
+      </q-item-section>
+    </q-item>
+  </q-list>
+  <q-separator/>
 </template>
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
