c625af68955df6cdd6238f80e84839efb76412a5
michaqu <michaqu11@wp.pl>
2023-01-16 23:30:16 +0100

    changed mechanism of saving user data

--- src/api/userInformation.ts
+++ src/api/userInformation.ts
@@ -13,12 +13,14 @@ const getInformation = (user: any) =>{
 
 const sendUserInformation = async (user: any) => {
     const information = getInformation(user);
-    try{
-        await api.post('/users', information)
-    }catch(err) {
-        console.log(err);
-    }
-    
+    return (api.post('/users', information)
+    .then((response)=> {
+        return response.data
+    }).catch((err)=>{
+        console.error(err)
+        return null
+    }))
+
 }
 
 export { sendUserInformation };
