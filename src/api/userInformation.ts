b108e29e20766086d70de7b4c542a091a064e749
michaqu <michaqu11@wp.pl>
2023-01-17 23:22:36 +0100

    refactor code, increase version and update quasar cli to 2.0.0

--- src/api/userInformation.ts
+++ src/api/userInformation.ts
@@ -13,14 +13,12 @@ const getInformation = (user: any) =>{
 
 const sendUserInformation = async (user: any) => {
     const information = getInformation(user);
-    return (api.post('/users', information)
-    .then((response)=> {
-        return response.data
-    }).catch((err)=>{
-        console.error(err)
+    try{
+        return (await api.post('/users', information)).data
+    }catch(err) {
+        console.error(err);
         return null
-    }))
-
+    }
 }
 
 export { sendUserInformation };
