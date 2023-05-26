f802c4c23fbb67e377e6376d30dbe8c01b444819
Michał Jasiński <michaqu11@wp.pl>
2023-05-27 01:47:23 +0200

    added overview tab with scroll-area component function project-overview page

--- src/components/project-overview/details/project-details.vue
+++ src/components/project-overview/details/project-details.vue
@@ -4,11 +4,14 @@
       <ProjectDetailsOverview
         v-if="props.type === $t('projectTabs.overview')"
       />
+
+      <ProjectDetailsCode v-if="props.type === $t('projectTabs.code')" />
     </q-card-section>
   </q-card>
 </template>
 
 <script setup lang="ts">
+import ProjectDetailsCode from './project-details-code.vue';
 import ProjectDetailsOverview from './project-details-overview.vue';
 
 const props = defineProps({
