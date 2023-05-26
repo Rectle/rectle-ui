32237f2d35cd0cc5634ad91c39f6ba4d472bdacf
Michał Jasiński <michaqu11@wp.pl>
2023-05-27 01:53:18 +0200

    added editor component to overview tab in poroject-details page

--- src/components/project-overview/details/project-details-overview.vue
+++ src/components/project-overview/details/project-details-overview.vue
@@ -1,7 +1,102 @@
 <template>
   <div class="q-ma-md">
     <q-scroll-area style="height: 500px">
-      <p><strong>What is a Getting Started competition?</strong></p>
+      <div class="q-pa-md q-gutter-sm">
+        <q-editor
+          v-model="editor"
+          :dense="$q.screen.lt.md"
+          :toolbar="[
+            [
+              {
+                label: $q.lang.editor.align,
+                icon: $q.iconSet.editor.align,
+                fixedLabel: true,
+                list: 'only-icons',
+                options: ['left', 'center', 'right', 'justify']
+              }
+            ],
+            [
+              'bold',
+              'italic',
+              'strike',
+              'underline',
+              'subscript',
+              'superscript'
+            ],
+            ['token', 'hr', 'link', 'custom_btn'],
+            ['print', 'fullscreen'],
+            [
+              {
+                label: $q.lang.editor.formatting,
+                icon: $q.iconSet.editor.formatting,
+                list: 'no-icons',
+                options: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'code']
+              },
+              {
+                label: $q.lang.editor.fontSize,
+                icon: $q.iconSet.editor.fontSize,
+                fixedLabel: true,
+                fixedIcon: true,
+                list: 'no-icons',
+                options: [
+                  'size-1',
+                  'size-2',
+                  'size-3',
+                  'size-4',
+                  'size-5',
+                  'size-6',
+                  'size-7'
+                ]
+              },
+              {
+                label: $q.lang.editor.defaultFont,
+                icon: $q.iconSet.editor.font,
+                fixedIcon: true,
+                list: 'no-icons',
+                options: [
+                  'default_font',
+                  'arial',
+                  'arial_black',
+                  'comic_sans',
+                  'courier_new',
+                  'impact',
+                  'lucida_grande',
+                  'times_new_roman',
+                  'verdana'
+                ]
+              },
+              'removeFormat'
+            ],
+            ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
+
+            ['undo', 'redo'],
+            ['viewsource']
+          ]"
+          :fonts="{
+            arial: 'Arial',
+            arial_black: 'Arial Black',
+            comic_sans: 'Comic Sans MS',
+            courier_new: 'Courier New',
+            impact: 'Impact',
+            lucida_grande: 'Lucida Grande',
+            times_new_roman: 'Times New Roman',
+            verdana: 'Verdana'
+          }"
+        />
+      </div>
+    </q-scroll-area>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// TODO: download overview text form databse by id
+// const props = defineProps({
+//   id: String
+// });
+
+const exampleText = `<p><strong>What is a Getting Started competition?</strong></p>
       <p>
         Getting Started competitions were created by Kaggle data scientists for
         people who have little to no machine learning background. They are a
@@ -73,14 +168,7 @@
       <p>
         If your problem persists or it seems to be effective all participants
         then please contact us.
-      </p>
-    </q-scroll-area>
-  </div>
-</template>
+      </p>`;
 
-<script setup lang="ts">
-// TODO: download overview text form databse by id
-// const props = defineProps({
-//   id: String
-// });
+const editor = ref(exampleText);
 </script>
