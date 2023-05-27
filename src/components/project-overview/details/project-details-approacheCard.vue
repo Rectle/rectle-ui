d9a5ce35fbf627ec943b6e149188aef41eba85d3
Michał Jasiński <michaqu11@wp.pl>
2023-05-27 02:35:50 +0200

    added overview apporaches tab in project-overview page

---
+++ src/components/project-overview/details/project-details-approacheCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-card class="q-pa-md">
+    <q-card-section horizontal>
+      <q-list class="my-list q-mr-sm">
+        <q-item v-ripple>
+          <q-item-section>
+            <q-icon name="fingerprint" size="2em" />
+          </q-item-section>
+
+          <q-item-section>{{ props.points }}</q-item-section>
+        </q-item>
+
+        <q-item v-ripple>
+          <q-item-section>
+            <q-icon name="o_analytics" size="2em" />
+          </q-item-section>
+
+          <q-item-section>{{ props.score }}</q-item-section>
+        </q-item>
+      </q-list>
+
+      <q-icon
+        class="q-ma-md q-ml-lg"
+        :name="status == 'done' ? 'o_check_circle' : 'o_pending'"
+        :color="status == 'done' ? 'green' : 'orange'"
+        size="4rem"
+      />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  points: Number,
+  score: Number,
+  status: String
+});
+</script>
+<style>
+.my-list .q-item__section--main + .q-item__section--main {
+  margin-left: 0px !important;
+}
+.my-list .q-item {
+  padding: 0px 5px;
+}
+</style>
--- src/components/project-overview/details/project-details.vue
+++ src/components/project-overview/details/project-details.vue
@@ -6,11 +6,16 @@
       />
 
       <ProjectDetailsCode v-if="props.type === $t('projectTabs.code')" />
+
+      <ProjectDetailsApproaches
+        v-if="props.type === $t('projectTabs.approaches')"
+      />
     </q-card-section>
   </q-card>
 </template>
 
 <script setup lang="ts">
+import ProjectDetailsApproaches from './project-details-approaches.vue';
 import ProjectDetailsCode from './project-details-code.vue';
 import ProjectDetailsOverview from './project-details-overview.vue';
 
