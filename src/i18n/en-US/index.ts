955cf7e91a76a62b0a049723c4d3554e15418815
Michał Jasiński <michaqu11@wp.pl>
2023-08-07 21:22:48 +0200

    moved description to i18n file

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -177,6 +177,55 @@ export default {
     },
     privacyAndSecurity: {
       title: 'Privacy & Security',
+      description: `Effective Date: 31.07.2023
+
+      Welcome to Rectle! This Privacy & Security Policy outlines how we collect, use, disclose, and protect your information when you use the Rectle application ("App"). Your privacy and security are of utmost importance to us, and we are committed to safeguarding your personal data. By using Rectle, you agree to the terms of this Privacy & Security Policy.
+
+      1. Information We Collect:
+
+      a. Personal Information: When you create an account on Rectle, we may collect certain personal information, including but not limited to your name, email address, and profile picture. This information is essential for providing you with a personalized experience and enabling specific features within the App.
+
+      b. Device Information: We may collect data about the device you use to access Rectle, such as the device's unique identifier, operating system, and mobile network information. This information helps us optimize our services for different devices and troubleshoot technical issues.
+
+      c. Usage Data: Our servers automatically record information about your interactions with Rectle, such as the pages you visit, the features you use, and the content you view. This data is collected anonymously and is used to improve the overall user experience and performance of the App.
+
+      2. How We Use Your Information:
+
+      a. Personalization: We use your personal information to personalize your experience with Rectle, provide relevant content, and suggest tailored recommendations.
+
+      b. Communication: We may use your email address to send you important updates, announcements, and promotional materials related to Rectle. You can opt-out of receiving marketing communications at any time.
+
+      c. Analytics: The usage data we collect is utilized for analytical purposes, enabling us to understand user behavior, preferences, and trends. This data helps us enhance the App's functionality and identify areas for improvement.
+
+      3. Data Sharing and Disclosure:
+
+      a. Third-party Service Providers: We may share your information with trusted third-party service providers that assist us in delivering our services, such as cloud storage providers, analytics platforms, and customer support tools. These entities are bound by strict confidentiality agreements and are not allowed to use your data for any other purpose.
+
+      b. Legal Requirements: We may disclose your information if required by law, such as to comply with a court order, legal process, or government request.
+
+      4. Data Security:
+
+      a. Encryption: We implement industry-standard encryption protocols to protect your personal data during transmission and storage.
+
+      b. Access Control: Access to your personal information is restricted to authorized personnel only, who need the data to perform their designated tasks.
+
+      c. Data Minimization: We retain your information only for as long as necessary to fulfill the purposes outlined in this policy.
+
+      5. Children's Privacy:
+
+      Rectle is not intended for use by individuals under the age of 13. We do not knowingly collect personal information from children. If you are a parent or guardian and believe that your child has provided us with personal information without your consent, please contact us immediately.
+
+      6. Changes to This Policy:
+
+      We reserve the right to update this Privacy & Security Policy from time to time. When we do so, we will notify you through the App or via the email address associated with your account. It is your responsibility to review this policy periodically to stay informed about any changes.
+
+      7. Contact Us:
+
+      If you have any questions, concerns, or requests regarding this Privacy & Security Policy or the practices of Rectle, please contact us at:
+
+      help@rectle.com
+
+      By using Rectle, you acknowledge that you have read and understood this Privacy & Security Policy and consent to the collection, use, and disclosure of your information as described herein.`,
     },
   },
 };
